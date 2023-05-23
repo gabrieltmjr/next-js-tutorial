c1403e7d4f6c0f3f63a2fbac5e5d3f3ec37ee882
gabrieltmjr <gabrieltmjr@gmail.com>
2023-05-23 17:11:38 +0100

    Assests, Metadata and CSS - CSS Styling & Layout Component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,10 +2,11 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
 import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
@@ -26,6 +27,6 @@ export default function FirstPost() {
             <h2>
             <Link href="/">Back to home</Link>
             </h2>
-        </>
+        </Layout>
       );
 }
