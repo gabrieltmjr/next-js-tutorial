14b8e539949aef4d4a3f1ae2db663abf45c59416
gabrieltmjr <gabrieltmjr@gmail.com>
2023-05-23 16:51:09 +0100

    Assests, Metadata and CSS - Metadata

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,21 +1,23 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import Head from 'next/head';
 
 export default function FirstPost() {
-
-
     return (
         <>
-          <Image
-          src="/images/profile.jpg" // Route of the image file
-          height={400} // Desired size with correct aspect ratio
-          width={400} // Desired size with correct aspect ratio
-          alt="Gabriel Machado Jr."
-          />
-          <h1>First Post</h1>
-          <h2>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <Image
+            src="/images/profile.jpg" // Route of the image file
+            height={400} // Desired size with correct aspect ratio
+            width={400} // Desired size with correct aspect ratio
+            alt="Gabriel Machado Jr."
+            />
+            <h1>First Post</h1>
+            <h2>
             <Link href="/">Back to home</Link>
-          </h2>
+            </h2>
         </>
       );
 }
